6f3acea80acf85f792c1bbdf77fd5e9b009fd05a
Julius Alvarado <angularjs.uiux@gmail.com>
2017-01-15 22:53:35 -0800

    cloned julius3d.com app
    am going to rebuild entire site to make it an angular app and don't want
    to break the original one.

---
+++ ja-modules/side-menu/side.menu.mod.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Julius Alvarado on 1/15/2017.
+ */
+
+
+
+//\\
---
+++ ja-modules/top-nav/top.nav.mod.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Julius Alvarado on 1/15/2017.
+ */
+
+(function(){
+    "use strict";
+
+    var app = angular.module('jaTopNav', ['jaSideMenu']);
+
+    app.directive('jaTopNav', [
+        function(){
+            return {
+                templateUrl: '',
+                link: function(){
+
+                }
+            }
+        }
+    ]);
+}());
+
+//\\
